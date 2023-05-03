eb83db37b9437d2fadafb6a867cd453320daf8ab
rgregory1 <mrgregory1@gmail.com>
2023-05-03 10:31:46 -0400

    added students page

--- src/loadPartial.js
+++ src/loadPartial.js
@@ -1,24 +1,19 @@
 function loadPartialHTML(partial) {
-  
-  const htmlServ = HtmlService.createTemplateFromFile(partial)
-  return htmlServ.evaluate().getContent()
-
+  const htmlServ = HtmlService.createTemplateFromFile(partial);
+  return htmlServ.evaluate().getContent();
 }
 
-function loadSearchView(){
-
-  return loadPartialHTML('search')
-  
+function loadSearchView() {
+  return loadPartialHTML("search");
 }
 
-function loadAddClassroomView(){
-
-  return loadPartialHTML('addClassroom')
-
+function loadAddClassroomView() {
+  return loadPartialHTML("addClassroom");
 }
 
-function loadEditClassroomView(){
-
-  return loadPartialHTML('editClassroom')
-
+function loadEditClassroomView() {
+  return loadPartialHTML("editClassroom");
+}
+function loadStudentView() {
+  return loadPartialHTML("students");
 }
