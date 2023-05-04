4be7fac748637eb3fba2737c51fbe3dc340599ba
rgregory1 <mrgregory1@gmail.com>
2023-05-04 13:00:23 -0400

    further work on student screen

--- src/serverSideFuncs.js
+++ src/serverSideFuncs.js
@@ -1,104 +1,137 @@
 function getDataForSearch() {
-  const ss = SpreadsheetApp.getActiveSpreadsheet()
-  const ws = ss.getActiveSheet()
+  const ss = SpreadsheetApp.getActiveSpreadsheet();
+  const ws = ss.getActiveSheet();
 
-  let data =  ws.getRange(2,1,ws.getLastRow()-1, 4).getValues()
-  return data
+  let data = ws.getRange(2, 1, ws.getLastRow() - 1, 4).getValues();
+  return data;
   // let rawData = ws.getDataRange().getValues()
   // let data = rawData.map(x => [x[0],x[1],x[3],x[6]])
-  
+
   // return data
   // return ws.getDataRange().getValues()
 }
 
-function archiveById (id){
-
+function archiveById(id) {
   // let id = '526390027173'
-  const ss = SpreadsheetApp.getActiveSpreadsheet()
-  const ws = ss.getActiveSheet()
+  const ss = SpreadsheetApp.getActiveSpreadsheet();
+  const ws = ss.getActiveSheet();
 
-  let courseIds =  ws.getRange(2,2,ws.getLastRow()-1, 1).getValues().map(r => r[0].toString())
+  let courseIds = ws
+    .getRange(2, 2, ws.getLastRow() - 1, 1)
+    .getValues()
+    .map(r => r[0].toString());
 
-  const posIndex = courseIds.indexOf(id.toString())
-  const rowNumber = posIndex === -1 ? 0 : posIndex + 2
+  const posIndex = courseIds.indexOf(id.toString());
+  const rowNumber = posIndex === -1 ? 0 : posIndex + 2;
 
-  archiveClass(id)
+  archiveClass(id);
 
-  ws.getRange(rowNumber, 4).setValue('ARCHIVED')
-  console.log('complete')
-
+  ws.getRange(rowNumber, 4).setValue("ARCHIVED");
+  console.log("complete");
 }
 
-function getCourseById(id='526390027173'){
+function getCourseById(id = "526390027173") {
+  let courseData = {};
 
-  let courseData = {}
+  const ss = SpreadsheetApp.getActiveSpreadsheet();
+  const ws = ss.getActiveSheet();
 
-  const ss = SpreadsheetApp.getActiveSpreadsheet()
-  const ws = ss.getActiveSheet()
+  let courseIds = ws
+    .getRange(2, 2, ws.getLastRow() - 1, 1)
+    .getValues()
+    .map(r => r[0].toString());
 
-  let courseIds =  ws.getRange(2,2,ws.getLastRow()-1, 1).getValues().map(r => r[0].toString())
-
-  const posIndex = courseIds.indexOf(id.toString())
-  const rowNumber = posIndex === -1 ? 0 : posIndex + 2
-  const courseInfo = ws.getRange(rowNumber,1,1,6).getValues()[0]
+  const posIndex = courseIds.indexOf(id.toString());
+  const rowNumber = posIndex === -1 ? 0 : posIndex + 2;
+  const courseInfo = ws.getRange(rowNumber, 1, 1, 6).getValues()[0];
 
   courseData = {
-    courseId: courseInfo[1], 
-    courseTeacher: courseInfo[2]
-  }
+    courseId: courseInfo[1],
+    courseTeacher: courseInfo[2],
+  };
 
-  courseData.courseName = getCourseData(id)
+  courseData.courseName = getCourseData(id);
 
-  courseData.teachers = getAllTeachers(courseData.courseId)
+  courseData.teachers = getAllTeachers(courseData.courseId);
 
-  courseData.alias = getAlias(courseData.courseId)
+  courseData.alias = getAlias(courseData.courseId);
 
-  courseData.teachers = courseData.teachers.filter(x => x !== courseData.courseTeacher)
+  courseData.teachers = courseData.teachers.filter(
+    x => x !== courseData.courseTeacher
+  );
 
-
-  return courseData
+  return courseData;
 }
-
-
 
 /**
  * simply archive one class when given the id
  */
-function archiveClass(thisClass){
-
-  Classroom.Courses.patch({courseState: "ARCHIVED"}, thisClass, {updateMask: "courseState"})
-
+function archiveClass(thisClass) {
+  Classroom.Courses.patch({ courseState: "ARCHIVED" }, thisClass, {
+    updateMask: "courseState",
+  });
 }
-
 
 /**
  * remove one teacher from a class
  */
-function removeCoteacherFromClass(classroomId, teacherId){
-
-  try{ 
-    let response = Classroom.Courses.Teachers.remove(classroomId, teacherId)
-  } catch (err){
-    return 'failed'
+function removeCoteacherFromClass(classroomId, teacherId) {
+  try {
+    let response = Classroom.Courses.Teachers.remove(classroomId, teacherId);
+  } catch (err) {
+    return "failed";
   }
-  return 'success'
+  return "success";
 }
 
 /**
  * add one teacher to a class
  */
-function addCoteacherToClass(classroomId='526390027173', teacherId='craig.davis@mvsdschools.org'){
-
-  try{
-    let response = Classroom.Courses.Teachers.create({userId: teacherId,}, classroomId) 
-  } catch(err){
-    return 'failed'
+function addCoteacherToClass(
+  classroomId = "526390027173",
+  teacherId = "craig.davis@mvsdschools.org"
+) {
+  try {
+    let response = Classroom.Courses.Teachers.create(
+      { userId: teacherId },
+      classroomId
+    );
+  } catch (err) {
+    return "failed";
   }
-  return 'success'
+  return "success";
 }
 
-function createCourse(data){
+/**
+ * add one student to a class
+ */
+function addStudentToClass(
+  classroomId = "526390027173",
+  studentId = "craig.davis@mvsdschools.org"
+) {
+  try {
+    let response = Classroom.Courses.Students.create(
+      {
+        userId: studentId,
+      },
+      classroomId
+    );
+  } catch (err) {
+    return "failed";
+  }
+  return "success";
+}
 
+removeStudentFromClass(student, courseId){
+  try {
+    let response = Classroom.Courses.Students.remove(courseId,student)
+  } catch (err) {
+    return "failed";
+  }
+  return "success";
+}
+
+function createCourse(data) {
   // data = {
   //   courseName : 'hello there4',
   //   teacherId : 'russell.gregory@mvsdschools.org',
@@ -106,37 +139,31 @@ function createCourse(data){
   // }
 
   let resource = {
-    "name": data.courseName,
-    "ownerId": data.teacherId
-    }
+    name: data.courseName,
+    ownerId: data.teacherId,
+  };
 
-  let courseData = Classroom.Courses.create(resource)
+  let courseData = Classroom.Courses.create(resource);
 
-  if (data.alias !== ''){
-
-      addAliasToAClass(thisAlias=data.alias, courseData=courseData.id)
+  if (data.alias !== "") {
+    addAliasToAClass((thisAlias = data.alias), (courseData = courseData.id));
   }
-  
+
   let provisionLine = [
     [
       courseData.name,
       courseData.id,
       data.teacherId,
       courseData.courseState,
-      '',
-      courseData.ownerId
-    ]
-  ]
+      "",
+      courseData.ownerId,
+    ],
+  ];
 
-  let provisionedSheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('Provisioned')
+  let provisionedSheet =
+    SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Provisioned");
 
-  provisionedSheet.insertRows(2)
+  provisionedSheet.insertRows(2);
 
-  provisionedSheet.getRange(2,1,1,6).setValues(provisionLine)
-
+  provisionedSheet.getRange(2, 1, 1, 6).setValues(provisionLine);
 }
-
-
-
-
-
--- src/utilities.js
+++ src/utilities.js
@@ -1,131 +1,126 @@
+function collectCourseInfo(courseId = "488854071820") {
+  let courseData = {};
 
-function collectCourseInfo(courseId='488854071820'){
+  courseData.teachers = getAllTeachers(courseId);
 
-  let courseData = {}
+  courseData.alias = getAlias(courseId);
 
-  courseData.teachers = getAllTeachers(courseId)
-
-  courseData.alias = getAlias(courseId)
-
-  return courseData
+  return courseData;
 }
 
+function getAlias(courseId) {
+  let alias = [];
+  let response = Classroom.Courses.Aliases.list(courseId);
 
-function getAlias(courseId){
-
-  let alias = []
-  let response = Classroom.Courses.Aliases.list(courseId)
-
-  if(response.aliases){
-    alias = response.aliases.map(x => x.alias)
+  if (response.aliases) {
+    alias = response.aliases.map(x => x.alias);
   }
-  
-  return alias
-}
 
+  return alias;
+}
 
 function getAllTeachers(courseId) {
-  
-  let response = Classroom.Courses.Teachers.list(courseId)   
+  let response = Classroom.Courses.Teachers.list(courseId);
 
-  let allTeachersList = response.teachers.map(x => x.profile.emailAddress)
+  let allTeachersList = response.teachers.map(x => x.profile.emailAddress);
 
-  return allTeachersList
-  
+  return allTeachersList;
 }
 
-function getCourseData(courseId = '526390027173'){
+function getCourseData(courseId = "526390027173") {
+  let response = Classroom.Courses.get(courseId);
 
-  let response = Classroom.Courses.get(courseId)
-
-  return response.name
+  return response.name;
 }
 
 /**
  *  remove one alias from a class
  */
-function removeAliasForOneClass(thisAlias,courseId){
+function removeAliasForOneClass(thisAlias, courseId) {
+  let response2 = Classroom.Courses.Aliases.remove(courseId, thisAlias);
 
-  let response2 = Classroom.Courses.Aliases.remove(courseId,thisAlias)
-
-  console.log(response2)
-  return 'success'
-
+  console.log(response2);
+  return "success";
 }
 
 /**
  * add alias to a class
  */
-function addAliasToAClass(thisAlias='d:new alias 1', courseId='526390027173'){
-
+function addAliasToAClass(
+  thisAlias = "d:new alias 1",
+  courseId = "526390027173"
+) {
   let alias = {
-    'alias' : thisAlias
-  }
-  
-  let response3 = Classroom.Courses.Aliases.create(resource=alias, courseId=courseId)
+    alias: thisAlias,
+  };
 
-  console.log(response3)
-  return 'success'
+  let response3 = Classroom.Courses.Aliases.create(
+    (resource = alias),
+    (courseId = courseId)
+  );
+
+  console.log(response3);
+  return "success";
 }
-
 
 /**
  * renames google classroom course
  */
-function changeClassroomName(id='526390027173', newName='howdy doody') {
-  
-  Classroom.Courses.patch({'name': newName}, id, {updateMask: "name"})
+function changeClassroomName(id = "526390027173", newName = "howdy doody") {
+  Classroom.Courses.patch({ name: newName }, id, { updateMask: "name" });
 
-  updateNameOnSS(id, newName)
-  
+  updateNameOnSS(id, newName);
 }
 
+function updateNameOnSS(courseid, newName) {
+  let currentSheet = SpreadsheetApp.getActive();
+  let currentSheetData = currentSheet.getDataRange().getValues();
 
-function updateNameOnSS(courseid, newName){
+  let lineIndex = currentSheetData.findIndex(x => x[1] == courseid);
 
-  let currentSheet = SpreadsheetApp.getActive()
-  let currentSheetData = currentSheet.getDataRange().getValues()
+  let lineToChange = parseInt(lineIndex) + 1;
 
-  let lineIndex = currentSheetData.findIndex(x => x[1] == courseid)
-
-  let lineToChange = parseInt(lineIndex) + 1
-  
-  currentSheet.getRange('A' + lineToChange).setValue(newName)
-
+  currentSheet.getRange("A" + lineToChange).setValue(newName);
 }
 
-
-function sortSheet(thisSheet, col = 5){
-
-  if(!thisSheet){
-    thisSheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet()
+function sortSheet(thisSheet, col = 5) {
+  if (!thisSheet) {
+    thisSheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
   }
-  
 
-  let range = thisSheet.getRange(2,1,thisSheet.getLastRow()-1, 6)
+  let range = thisSheet.getRange(2, 1, thisSheet.getLastRow() - 1, 6);
 
-  let data = range.getValues()
+  let data = range.getValues();
 
-  data.sort(function(a,b){
-    return b[col-1] - a[col-1]
-  })
-
+  data.sort(function (a, b) {
+    return b[col - 1] - a[col - 1];
+  });
 
   // thisSheet.sort({column: 4, ascending: false})
 
-  range.setValues(data)
+  range.setValues(data);
 
   // range.sort()
 }
 
+function getStudentData(courseId = "595042513070") {
+  let studentData = {};
 
+  studentData.courseId = courseId;
+  studentData.name = getCourseData(courseId);
+  studentData.studentList = []
 
+  let response = Classroom.Courses.Students.list(courseId);
+ 
+  if (Object.keys(response).length === 0){
+    studentData.studentList = []
+    
+  } else {
+    studentData.studentList = response.students.map(x => x.profile.emailAddress);
+  }
+  
+  
+  
 
-
-
-
-
-
-
-
-
+  return studentData;
+}
